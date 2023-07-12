f991b30248366a1e159478292e88614410928e06
Adrian Żegnalek <zegnalekadrian@outlook.com>
2023-07-12 21:37:09 +0200

    fixes v5

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,10 @@ app.use(bodyParser.json());
 // app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://lunarweb.netlify.app/, https://lunarweb.dev/'
+  );
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, PATCH, DELETE'
