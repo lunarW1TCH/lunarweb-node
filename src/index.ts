1fb2b56273dc3e8426484eaef5f26682dad46829
Adrian Żegnalek <zegnalekadrian@outlook.com>
2023-07-12 21:31:46 +0200

    fixes v3

--- src/index.ts
+++ src/index.ts
@@ -33,6 +33,15 @@ app.use(bodyParser.json());
 
 // app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+});
+
 app.post(
   '/mail',
   [
