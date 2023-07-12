192c293e38ec84b087f34e453af055ba997ab915
Adrian Żegnalek <zegnalekadrian@outlook.com>
2023-07-12 21:28:55 +0200

    fixes v2

--- src/index.ts
+++ src/index.ts
@@ -23,15 +23,15 @@ const transporter = createTransport({
 const app = express();
 app.use(bodyParser.json());
 
-const corsOptions: CorsOptions = {
-  allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin'],
-  methods: ['GET', 'POST', 'OPTIONS'],
-  origin: ['https://lunarweb.netlify.app/', 'https://lunarweb.dev/'],
-};
+// const corsOptions: CorsOptions = {
+//   allowedHeaders: ['Content-Type', 'Access-Control-Allow-Origin'],
+//   methods: ['GET', 'POST', 'OPTIONS'],
+//   origin: ['https://lunarweb.netlify.app/', 'https://lunarweb.dev/'],
+// };
 
-app.options('*', cors());
+// app.options('*', cors());
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.post(
   '/mail',
